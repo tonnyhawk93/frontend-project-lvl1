747c75b9d1a554c8d53201b20e291403e21dbd71
tonnyhawk <tonnyhawk93@gmail.com>
2022-01-30 00:59:47 +0300

    stage 3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import readLineSync from "readline-sync";
 console.log("Welcome to the Brain Games!");
+const name = readLineSync.question('May I have your name? ');
+console.log(`Hello, ${name}!`);
