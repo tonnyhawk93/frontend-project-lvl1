6d5fc66509cf8762f5f2057c8e47d4c7d4b8e237
tonnyhawk <tonnyhawk93@gmail.com>
2022-02-02 15:15:39 +0300

    stage 4

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ brain-games:
 	node bin/brain-games.js
 publish:
 	npm publish --dry-run
+lint:
+	npx eslint .
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import readLineSync from "readline-sync";
-console.log("Welcome to the Brain Games!");
+import readLineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
 const name = readLineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
